aaa276643b1c2ac8a11a39573f251b8ca778d8b4
Ngô Đức Xuân <xuan.ngoduc@axonactive.com>
2017-12-22 15:29:14 +0700

    yes

---
+++ mining_t1.bat
@@ -0,0 +1,2 @@
+xmrig.exe -a cryptonight -o stratum+tcp://monerohash.com:3333 -u 4AAm2DMGhw3GT4sZUgzBtW6tvMQhsbBiyFjSEFTbPo342xexMfnsrayWbwaVe4vUMveKAzAiA4j8xgUi29TpKXpm41HQFqH -p x -t 1
+
---
+++ mining_t3.bat
@@ -0,0 +1,2 @@
+xmrig.exe -a cryptonight-lite -o mine.aeon-pool.com:5555 -u WmtioSvnUZKYPsCzXUjs3oHL7wQRRmVzGSZo52sP4hYVUF5XS8yU4UiF1K23tYQ3NnJ24841s95Y3RZ9u98AQ3PG1wyVYwLM2 -p x -t 3
+
